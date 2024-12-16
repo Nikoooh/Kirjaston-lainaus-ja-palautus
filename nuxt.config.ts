78f89ba1750ae0d58d47031cdf564ea455e953e5
Nikoooh <nikotoivanen1999@gmail.com>
2024-12-16 15:42:15 +0200

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxthub/core'],
+  ssr: true,
+  modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
       SUPABASE_URL: process.env.SUPABASE_URL,
       SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
     }
-  }
+  },
 })
